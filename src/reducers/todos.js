cff06931a33730643aed7a782c35670a8581d1f5
ditch0 <ditch0@users.noreply.github.com>
2018-06-02 19:22:40 +0300

    Change todos state structure

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,22 +1,35 @@
 import todo from './todo'
 import { ADD_TODO, TOGGLE_TODO } from '../actions';
+import { combineReducers } from 'redux';
 
-export default (state = [], action) => {
+const byId = (state = {}, action) => {
     switch (action.type) {
         case ADD_TODO:
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
         case TOGGLE_TODO:
-            return state.map((item) => todo(item, action));
+            return {
+                ...state,
+                [action.id]: todo(state[action.id], action)
+            };
         default:
             return state
     }
 };
 
+const allIds = (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return [...state, action.id];
+        default:
+            return state;
+    }
+};
+
+export default combineReducers({ byId, allIds });
+
+const getAllTodos = ({ byId, allIds }) => allIds.map((id) => byId[id]);
+
 export const filterVisibleTodos = (state, filter) => {
-    return state.filter((todo) => {
+    return getAllTodos(state).filter((todo) => {
         switch (filter) {
             case 'completed':
                 return todo.completed;
--- src/reducers/todos.test.js
+++ src/reducers/todos.test.js
@@ -2,56 +2,72 @@ import deepFreeze from 'deep-freeze'
 import todos, { filterVisibleTodos } from './todos'
 
 describe('todos reducer', () => {
-    it('should initialize state with empty list', () => {
-        expect(todos(undefined, {})).toEqual([]);
+    it('should initialize state', () => {
+        expect(todos(undefined, {})).toEqual({ byId: {}, allIds: [] });
     });
 
     it('should add todo to list', () => {
-        const stateBefore = [
-            {
-                id: 1,
-                text: 'first todo',
-                completed: true
-            }
-        ];
-        const stateAfter = [
-            ...stateBefore,
-            {
-                id: 2,
-                text: 'second todo',
-                completed: false
-            }
-        ];
+        const stateBefore = {
+            byId: {
+                1: {
+                    id: 1,
+                    text: 'first todo',
+                    completed: true
+                }
+            },
+            allIds: [1]
+        };
+        const stateAfter = {
+            byId: {
+                1: {
+                    id: 1,
+                    text: 'first todo',
+                    completed: true
+                },
+                2: {
+                    id: 2,
+                    text: 'second todo',
+                    completed: false
+                }
+            },
+            allIds: [1, 2]
+        };
 
         deepFreeze(stateBefore);
         expect(todos(stateBefore, { type: 'ADD_TODO', id: 2, text: 'second todo' })).toEqual(stateAfter)
     });
 
     it('should toggle todo', () => {
-        const stateBefore = [
-            {
-                id: 1,
-                text: 'first todo',
-                completed: true
+        const stateBefore = {
+            byId: {
+                1: {
+                    id: 1,
+                    text: 'first todo',
+                    completed: true
+                },
+                2: {
+                    id: 2,
+                    text: 'second todo',
+                    completed: false
+                }
             },
-            {
-                id: 2,
-                text: 'second todo',
-                completed: false
-            }
-        ];
-        const stateAfter = [
-            {
-                id: 1,
-                text: 'first todo',
-                completed: true
+            allIds: [1, 2]
+        };
+        const stateAfter = {
+            byId: {
+                1: {
+                    id: 1,
+                    text: 'first todo',
+                    completed: true
+                },
+                2: {
+                    id: 2,
+                    text: 'second todo',
+                    completed: true
+                }
             },
-            {
-                id: 2,
-                text: 'second todo',
-                completed: true
-            }
-        ];
+            allIds: [1, 2]
+        };
 
         deepFreeze(stateBefore);
         expect(todos(stateBefore, { type: 'TOGGLE_TODO', id: 2 })).toEqual(stateAfter)
@@ -69,22 +85,28 @@ describe('filterVisibleTodos', () => {
         text: 'second todo',
         completed: false
     };
-    const allTodos = [completedTodo, uncompletedTodo];
-    deepFreeze(allTodos);
+    const state = {
+        byId: {
+            [completedTodo.id]: completedTodo,
+            [uncompletedTodo.id]: uncompletedTodo
+        },
+        allIds: [completedTodo.id, uncompletedTodo.id]
+    };
+    deepFreeze(state);
 
     it('should show completed todos', () => {
-        expect(filterVisibleTodos(allTodos, 'completed')).toEqual([completedTodo]);
+        expect(filterVisibleTodos(state, 'completed')).toEqual([completedTodo]);
     });
 
     it('should show uncompleted todos', () => {
-        expect(filterVisibleTodos(allTodos, 'uncompleted')).toEqual([uncompletedTodo]);
+        expect(filterVisibleTodos(state, 'uncompleted')).toEqual([uncompletedTodo]);
     });
 
     it('should show all todos', () => {
-        expect(filterVisibleTodos(allTodos, 'all')).toEqual(allTodos);
+        expect(filterVisibleTodos(state, 'all')).toEqual([completedTodo, uncompletedTodo]);
     });
 
     it('should throw an exception for unsupported filter value', () => {
-        expect(() => filterVisibleTodos(allTodos, 'XXX')).toThrowError();
+        expect(() => filterVisibleTodos(state, 'XXX')).toThrowError();
     });
 });
